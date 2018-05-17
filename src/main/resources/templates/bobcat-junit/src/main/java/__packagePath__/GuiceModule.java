b4983a73284df6b2b8db93354fa9ca20ade58069
Daniel Madejek <daniel.madejek@cognifide.com>
2018-05-17 15:36:52 +0200

    #235 bobcat simple bdd moved to gradle

--- src/main/resources/templates/bobcat-junit/src/main/java/__packagePath__/GuiceModule.java
+++ src/main/resources/templates/bobcat-junit/src/main/java/__packagePath__/GuiceModule.java
@@ -16,9 +16,6 @@ import com.google.inject.AbstractModule;
 public class GuiceModule extends AbstractModule {
   @Override
   protected void configure() {
-    if (System.getProperty("configuration.paths") == null) {
-      System.setProperty("configuration.paths", "src/main/config/dev;src/main/config/common");
-    }
     install(new CoreModule());
     install(new ReporterModule());
   }
