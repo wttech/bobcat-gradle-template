6c56dca1fc0394821ff393c2836013f853840fef
Daniel Madejek <dmadejek@gmail.com>
2018-07-02 22:30:14 +0200

    #235 appium template finished

--- src/main/resources/templates/bobcat-appium/src/main/java/__packagePath__/guice/GuiceModule.java
+++ src/main/resources/templates/bobcat-appium/src/main/java/__packagePath__/guice/GuiceModule.java
@@ -7,9 +7,6 @@ import com.google.inject.AbstractModule;
 public class GuiceModule extends AbstractModule {
 
   public void configure() {
-    if (System.getProperty("configuration.paths") == null) {
-      System.setProperty("configuration.paths", "src/main/config/dev;src/main/config/common");
-    }
     install(new CoreModule());
     install(new ReporterModule());
   }
