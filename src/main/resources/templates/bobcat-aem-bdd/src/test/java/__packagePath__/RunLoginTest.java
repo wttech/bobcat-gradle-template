c0e628915bc7198d59621536fc4c1fcca6b2d944
Daniel Madejek <daniel.madejek@cognifide.com>
2018-08-07 12:34:48 +0200

    #274 compatible with new cucumber version

--- src/main/resources/templates/bobcat-aem-bdd/src/test/java/__packagePath__/RunLoginTest.java
+++ src/main/resources/templates/bobcat-aem-bdd/src/test/java/__packagePath__/RunLoginTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/features/login", format = { "pretty",
+@CucumberOptions(features = "src/main/features/login", plugin = { "pretty",
 		"html:target/cucumber-login-html-report", "json:target/cucumber-login-report.json" })
 public class RunLoginTest {
 	// empty
--- src/main/resources/templates/bobcat-bdd/src/test/java/__packagePath__/RunWikipediaTest.java
+++ src/main/resources/templates/bobcat-bdd/src/test/java/__packagePath__/RunWikipediaTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/features/example", format = {"pretty",
+@CucumberOptions(features = "src/main/features/example", plugin = {"pretty",
     "html:target/cucumber-wikipedia-html-report", "json:target/cucumber-wikipedia-report.json"})
 public class RunWikipediaTest {
   // empty
