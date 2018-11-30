0116051585ea13603790eb753d34542513434485
Daniel Madejek <daniel.madejek@cognifide.com>
2018-11-30 14:36:31 +0100

    AEM JUNIT

--- src/main/resources/templates/bobcat-aem-junit/src/test/java/__packagePath__/login/AuthorizationTest.java
+++ src/main/resources/templates/bobcat-aem-junit/src/test/java/__packagePath__/login/AuthorizationTest.java
@@ -1,39 +1,44 @@
 package @packageName@.login;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.AfterClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.cognifide.qa.bb.junit.Modules;
-import com.cognifide.qa.bb.junit.TestRunner;
-import com.cognifide.qa.bb.provider.selenium.BobcatWait;
-import @packageName@.GuiceModule;
-import @packageName@.pageobjects.login.LoginPage;
+import com.cognifide.qa.bb.aem.core.api.AemActions;
+import com.cognifide.qa.bb.api.actions.ActionException;
+import com.cognifide.qa.bb.api.actions.ActionsController;
+import com.cognifide.qa.bb.junit5.guice.Modules;
+import com.cognifide.qa.bb.modules.BobcatRunModule;
+import com.cognifide.qa.bb.page.BobcatPageFactory;
 import com.google.inject.Inject;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+import @packageName@.pages.TestPage;
 
 /**
  * Test for presenting how to log in to the page.
  */
-@RunWith(TestRunner.class)
-@Modules(GuiceModule.class)
+@Modules(BobcatRunModule.class)
+@Epic("AEM 6.4 Base Tests")
+@Feature("Login to AEM")
 public class AuthorizationTest {
 
 	@Inject
-	private LoginPage loginPage;
+	private ActionsController controller;
+
+	@Inject
+	private BobcatPageFactory bobcatPageFactory;
 
 	@Test
-	public void loginAsAuthorTest() {
-		assertTrue("Login page is not displayed", loginPage.loginAsAuthor().isDisplayed());
-	}
-
-	/**
-	 * After all tests sleep 1 second to let webDriver close the window
-	 */
-	@AfterClass()
-	public static void afterClass() {
-		BobcatWait.sleep(1);
+	@Story("Login to AEM and open Test page")
+	@Description("Login to author instance and open test page")
+	public void loginTest() throws ActionException {
+		TestPage testpage = bobcatPageFactory.create("/content/we-retail/us/en.html", TestPage.class);
+		controller.execute(AemActions.LOG_IN);
+		assertTrue(testpage.open().isDisplayed());
 	}
 
 }
